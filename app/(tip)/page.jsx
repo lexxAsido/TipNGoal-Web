e12e8d9632df39b7893d8ecb7cb7602dfd920e18
lexxAsido <alexandaras2015@gmail.com>
2025-01-28 14:55:50 +0100

    new commit

--- app/(tip)/page.jsx
+++ app/(tip)/page.jsx
@@ -17,7 +17,7 @@ const page = () => {
     <main className="bg-black">
       <div className=" flex flex-col lg:flex-row justify-center items-center gap-10 md:gap-20  mx-5">
         <Image
-          src="/tipnews.png"
+          src="/tipfront.jpg"
           alt="TipNGoal Prediction"
           width={800}
           height={100}
