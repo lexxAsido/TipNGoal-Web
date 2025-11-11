62f2e4cc6f7aea019cb73cef29df1728ef2b8d1d
lexxAsido <alexandaras2015@gmail.com>
2025-11-11 21:13:51 +0100

    Fix sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://tipngoal.com',
+  siteUrl: 'https://tipngoal.com', // your live domain
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  exclude: ['/404'], // optional, exclude error pages
+  generateIndexSitemap: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  transform: async (config, path) => {
+    // Customize each URL if needed
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
 };
